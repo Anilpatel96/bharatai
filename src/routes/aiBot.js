b66f1731aea560fc16ad7e9163e416e789fcf339
Anksji <ankitrajdwivedi@gmail.com>
2023-11-23 19:55:01 +0530

    added ai prompt support

--- src/routes/aiBot.js
+++ src/routes/aiBot.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const {OpenAiChat}=require("../controllers/aiBotController");
+const {addAiPrompt,updateAiPrompt,deleteAiPrompt,getAllAiPrompt}=require("../controllers/aiPromptController");
 const myAuth=require("../middlewares/auth");
 const aiRouter=express.Router();
 const dotenv = require('dotenv');
@@ -10,6 +11,10 @@ dotenv.config();
 
 aiRouter.post("/"+process.env.PATH_KEY,myAuth,OpenAiChat);
 
-aiRouter.post("/prompts",myAuth,OpenAiChat);
+///ai prompt operations
+aiRouter.post("/prompts",myAuth,addAiPrompt);
+aiRouter.put("/prompts",myAuth,updateAiPrompt);
+aiRouter.delete("/prompts",myAuth,deleteAiPrompt);
+aiRouter.get("/prompts",myAuth,getAllAiPrompt);
 
 module.exports=aiRouter;
